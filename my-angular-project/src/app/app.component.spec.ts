a5a68773d994aead7eac530e6153ead485e9b3c3
Tedi5666 <tedifirkov@gmail.com>
2024-03-19 18:45:07 +0200

    Make a core module and a nav component

--- my-angular-project/src/app/app.component.spec.ts
+++ my-angular-project/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-angular-project');
   });
-
+  
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
---
+++ my-angular-project/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    CatalogComponent,
+    EditComponent,
+    DetailsComponent
+
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule
+
+  ],
+  providers: [],
+
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
